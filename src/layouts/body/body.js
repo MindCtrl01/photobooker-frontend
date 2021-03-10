06203565c68105eb9d73ec2006a1dc8b5a486303
TuanAnh <4evernavi242@gmail.com>
2021-03-10 22:02:07 +0700

    test

--- src/layouts/body/body.js
+++ src/layouts/body/body.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import { Route, Switch } from 'react-router'
 import { BrowserRouter as Router } from 'react-router-dom'
 import routes from '../../router/index'
@@ -6,15 +6,15 @@ import routes from '../../router/index'
 function Body() {
   console.log(routes)
   return (
-      <Router  basename={'/photobooker-frontend/'}>
-        <Switch>
-          {
-            routes.map((route, i) => (
-              <Route key={i} path={route.path} component={route.component} />
-            ))
-          }
-        </Switch>
-      </Router>
+    <Router basename={'/photobooker-frontend/'}>
+      <Switch>
+        {
+          routes.map((route, i) => (
+            <Route key={i} path={route.path} component={route.component} />
+          ))
+        }
+      </Switch>
+    </Router>
   )
 }
 
